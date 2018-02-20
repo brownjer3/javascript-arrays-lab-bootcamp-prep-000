8b31a6f3424bde3e28c4f7a49b37934391c3958f
Jerry Brown <brownjer3@gmail.com>
2018-02-20 23:53:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-const app = "I don't do much."
+const app = "I don't do much."; 
+
+var kittens = ['Milo', 'Otis', 'Garfield'];
+
+function destructivelyAppendKitten(name) {
+  return kittens.push(name);
+}
+
+function destructivelyPrependKitten(name) {
+  return kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten() {
+  return kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten() {
+  return kittens.shift();
+}
+
+function appendKitten(name) {
+  kittens(...kittens, name);
+  return kittens;
+}
